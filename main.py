f9e95ab1f88656b2d0fc47af3f6373f972ebb1a8
PC <qrabeq@gmail.com>
2021-07-10 18:55:06 +0300

    edge detection

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     # Считывание всех имен нужного формата в папке данных
     os.chdir('data1')
     directories = 'raw'
-    print(os.getcwd())
+    #print(os.getcwd())
     file_format = '.bmp'
     frames = []
     for file_name in os.listdir(directories):
@@ -26,14 +26,36 @@ if __name__ == '__main__':
             frames.append(Snap(os.getcwd() + '\\' + directories + '\\' + file_name))
     # Проверка наличия папок и создание
     directories = ('crop', 'subs', 'threshold', 'edge', 'detected')
-    for i in directories:
-        if i in os.listdir(os.getcwd()):
-            os.rmdir(i)
-        os.mkdir(i)
-        for f in frames:
-            f.path[i] = os.getcwd() + '\\' + i + '\\' + f.name
-    im = cv2.imread(frames[0].path['raw'])[:, :, 0]
-    cr = im[0:320, 0:1000]
+    for directory in directories:
+        if directory in os.listdir(os.getcwd()):
+            for file in os.listdir(os.getcwd() + '\\' + directory):
+                os.remove(os.getcwd() + '\\' + directory + '\\' + file)
+                print(directory, ' ', file, ' ', 'has been removed')
+            os.rmdir(directory)
+        os.mkdir(directory)
+        for frame in frames:
+            frame.path[directory] = os.getcwd() + '\\' + directory + '\\' + frame.name
+    # Обрезка и запись изображений
+    for frame in frames:
+        cr = cv2.imread(frame.path['raw'])[:, 0:1000, 0]
+        cv2.imwrite(frame.path['crop'], cr)
+    # Поиск минимумов
+    background = cv2.imread(frames[0].path['crop'])[:, :, 0]
+    for frame in frames:
+        background = np.minimum(cv2.imread(frame.path['crop'])[:, :, 0], background)
+        #comparison = cv2.imread(frame.path['crop'])[:, :, 0] > background
+    cv2.imwrite(os.getcwd() + '\\' + 'background.bmp', background)
+    # Вычитание фона
+    for frame in frames:
+        subs = cv2.imread(frame.path['crop'])[:, :, 0] - background
+        cv2.imwrite(frame.path['subs'], subs)
 
 
+    for frame in frames:
+        edge = cv2.imread(frame.path['subs'])[:, :, 0]
+        edge = cv2.Canny(edge, 40, 150)
+        cv2.imwrite(frame.path['edge'], edge)
+
+    cv2.imshow('a', edge)
+    cv2.waitKey(0)
 
